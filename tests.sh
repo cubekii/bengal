4aecfbbaf8ede531d2efed2e0dc8d51d5242475b
Cubek <manak47real@gmail.com>
2026-03-10 17:52:19 +0300

    rewrite something

--- tests.sh
+++ tests.sh
@@ -3,8 +3,8 @@ cd "$(dirname "$0")"
 
 test=1
 
-for i in $(ls example); do
-  target/release/bengal example/$i > /dev/null || { test=0; echo -e "\e[31m$i finished with error\e[0m"; }
+for i in example/*; do
+  target/release/bengal $i > /dev/null || { test=0; echo -e "\e[31m$i finished with error\e[0m"; }
 done
 
 if [ $test -eq 0 ]; then
