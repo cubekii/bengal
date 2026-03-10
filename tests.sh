c535d02d29a1dc4bcf375a7b9fadb737e6df8e7b
Cubek <manak47real@gmail.com>
2026-03-10 14:49:20 +0300

    update tests.sh and pipeline

--- tests.sh
+++ tests.sh
@@ -2,11 +2,12 @@
 cd "$(dirname "$0")"
 
 test=1
+set -o pipefail
 
 for i in $(ls example); do
-  target/release/bengal example/$i > /dev/null || (echo -e "\e[31m$i finished with error \e[0m" && test=0)
+  target/release/bengal example/$i > /dev/null || { test=0; echo -e "\e[31m$i finished with error\e[0m"; }
 done
 
 if [ $test == 0 ]; then
-  exit 1
+  exit
 fi
